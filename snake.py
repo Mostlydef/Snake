0e55c95c395ad44baa0deb429d570733f0c712ec
Username1336 <2blessthanthree@mail.ru>
2021-05-02 18:38:09 +0300

    Initial commit.

---
+++ snake.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Snake:
+    def __init__(self):
+        self.setup()
+
+    def setup(self):
+        pass
+
+    def run(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def move(self):
+        pass
+
+    def spawn_food(self):
+        pass
