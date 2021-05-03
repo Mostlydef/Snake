9e0e841e6deb0a3e74a94cab4cd50c4348a2351d
Username1336 <2blessthanthree@mail.ru>
2021-05-03 17:06:41 +0300

    feat: Добавление фич для класса Snake, логики главного меню
     - Добавил реализацию метода run.
    
     - Добавил логику главного меню.

---
+++ Command.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+import snake
+import os
+import global_variables as gv
+import pygame
+
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+
+class StartTheGameCommand(Command):
+    def execute(self) -> None:
+        game = snake.Snake()
+        game.run()
+
+
+class HelpCommand(Command):
+    def execute(self) -> None:
+        os.system("hh.exe help.chm")
+
+
+class ChangeVolumeCommand(Command):
+    def __init__(self):
+        self.value = True
+
+    def execute(self) -> None:
+        gv.volume = self.value
+
+
+class ChangeMusicVolumeCommand(Command):
+    def __init__(self):
+        self.value = True
+
+    def execute(self) -> None:
+        if not self.value:
+            pygame.mixer.music.pause()
+        else:
+            pygame.mixer.music.unpause()
+
+
+class Invoker:
+    def __init__(self, start_the_game, help, change_volume, change_music_value):
+        self._start_the_game = start_the_game
+        self._help = help
+        self._change_volume = change_volume
+        self._change_music_value = change_music_value
+
+    def start_the_game(self):
+        self._start_the_game.execute()
+
+    def help(self):
+        self._help.execute()
+
+    def change_volume(self, string, value):
+        self._change_volume.value = value
+        self._change_volume.execute()
+
+    def change_music_volume(self, string, value):
+        self._change_music_value.value = value
+        self._change_music_value.execute()
+
+    def change_resolution(self, string, value):
+        self._change_resolution.value = value
+        self._change_resolution.execute()
+
+
+invoker = Invoker(StartTheGameCommand(), HelpCommand(), ChangeVolumeCommand(), ChangeMusicVolumeCommand())
